d38e50828c1e3d422f92103d1a8b307356624ddd
Henrik Obsen <hto@e-train.dk>
2018-03-26 12:35:26 +0200

    er

---
+++ EDMX_Test/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDMX_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            //var t = "";
+
+            //foreach (var item in courses)
+            //{
+            //    t += "<li>" + item.Title + "</li>";
+            //}
+
+            //ViewBag.MSG = t;
+            return View(dbContext.Courses.ToList());
+        }
+    }
+}
