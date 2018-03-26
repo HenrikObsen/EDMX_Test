e8112c528901b4acc1b324690ff41e53db569ec3
Henrik Obsen <hto@e-train.dk>
2018-03-26 08:39:04 +0200

    ff

--- EDMX_Test/PlutoModel.Designer.cs
+++ EDMX_Test/PlutoModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\hto\Source\Repos\EDMX_Test\EDMX_Test\PlutoModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hto\Source\Repos\EDMX_Test\EDMX_Test\PlutoModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
